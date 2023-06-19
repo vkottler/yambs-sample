72e95b46f73b50375e540f69952b52b1889f5867
Vaughn Kottler <vaughnkottler@gmail.com>
2023-06-19 00:44:38 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+<!--
+    =====================================
+    generator=datazen
+    version=3.1.2
+    hash=e2310ac44588a494a8dce953fe893df8
+    =====================================
+-->
+
+# yambs-sample
+
+A sample yambs project.
---
+++ src/example/sample.cc
@@ -0,0 +1,19 @@
+#include "example/sample.h"
+
+namespace Example {
+
+void method1(void) {
+  int a = 0;
+  for (int i = 0; i < 1000; i++) {
+    a *= 2;
+  }
+}
+
+void method2(void) {
+  int a = 0;
+  for (int i = 0; i < 1000; i++) {
+    a *= 2;
+  }
+}
+
+} // namespace Example
